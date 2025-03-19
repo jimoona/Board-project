97f69ac12803fd1355f8ab960beafc98a45f0490
황지윤 <hwangjiyoon@hwangjiyun-ui-noteubug.local>
2025-03-19 17:11:03 +0900

    글 작성기능 만들기

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public')); //public폴더의 static파일들 사용
-app.set('view engine', 'ejs') //템플릿엔진 사용
+app.set('view engine', 'ejs') //템플릿엔진 사용(DB의 데이터를 ejs파일에 꽂아넣기)
+app.use(express.json()) //요청.body를 쓰기 위한 세팅
+app.use(express.urlencoded({extended:true})) //요청.body
 
 const { MongoClient } = require('mongodb')
 
@@ -22,9 +24,20 @@ new MongoClient(url).connect().then((client)=>{
   console.log(err)
 })
 //몽고디비 라이브러리 셋팅
-app.get('/', (요청, 응답) =>{
+app.get('/', (요청, 응답) => {
      응답.send('Hello, world!')
-   })
-app.get('/list', (요청, 응답) =>{
-    응답.render('list.ejs')
-  })
+})
+app.get('/list', async(요청, 응답) => {
+    let result = await db.collection('post').find().toArray() //DB에서 데이터 가져오기
+    console.log(result)
+    응답.render('list.ejs', {글목록 : result}) //서버 데이터를 ejs 파일로 전송, 데이터의 이름은 글목록
+})
+app.get('/write', (요청, 응답) => {
+    응답.render('write.ejs')
+})
+app.post('/add', async(요청, 응답) => {
+    console.log(요청.body) //유저가 보낸 데이터 출력 가능, {title: '글제목', content: '글내용'} 형태의 데이터
+    await db.collection('post').insertOne({title : 요청.body.title, content : 요청.body.content})
+    //몽고디비의 post 컬렉션에 title과 content라는 키로 데이터를 저장
+    응답.redirect('/list') //서버실행 후 유저를 다른 페이지로 이동시킴
+})
